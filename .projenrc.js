28b54dce811d5ec3154e1c6ed65646a95c884de8
Steyn Huizinga <steyn@oblcc.com>
2022-03-14 21:23:36 +0100

    Upgrade cdk version and add gitignore

--- .projenrc.js
+++ .projenrc.js
@@ -1,9 +1,10 @@
 const { awscdk } = require('projen');
 const project = new awscdk.AwsCdkTypeScriptApp({
-  cdkVersion: '2.1.0',
+  cdkVersion: '2.16.0',
   defaultReleaseBranch: 'main',
   name: 'gitlab-aws-oidc',
-
+  vscode: true,
+  gitignore: [ '.vscode/' ],
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
