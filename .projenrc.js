c28910dc6c0c66145cc0417c0f05bee0725d47c1
Steyn Huizinga <steyn@oblcc.com>
2022-05-11 09:31:04 +0200

    Disable github

--- .projenrc.js
+++ .projenrc.js
@@ -1,10 +1,12 @@
 const { awscdk } = require('projen');
 const project = new awscdk.AwsCdkTypeScriptApp({
-  cdkVersion: '2.16.0',
+  cdkVersion: '2.23.0',
+  cdkVersionPinning: true,
   defaultReleaseBranch: 'main',
   name: 'gitlab-aws-oidc',
   vscode: true,
   gitignore: [ '.vscode/' ],
+  github: false,
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
